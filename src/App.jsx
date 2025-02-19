22912534df99e63bfc8bb3f2496a6f9e3d47c37e
Aneeshraikwar <aneeshraikwar@gmail.com>
2025-02-19 16:29:58 +0530

    add loading state and Redux dispatch in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-
+import { useState ,useEffect} from 'react'
 import './App.css'
 import config from './conf/config'
+import { useDispatch } from 'react-redux'
+import authService from './Appwrite/auth'
+
 
 function App() {
- 
+const [Loading,setLoading]= useState(true)
+const dispatch = useDispatch()
   return (
     <>
      <h1>hello {config.VITE_APWRITE_URL}</h1>
